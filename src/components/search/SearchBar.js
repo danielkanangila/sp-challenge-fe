a7410f0818766c43ba7cd1d5a74eab37f4f211f1
danielkanangila <daniel.kanangila@gmail.com>
2020-12-18 01:30:55 -0500

    added show total result of search

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -7,22 +7,30 @@ import styles from "./../../styles/search.module.scss";
 
 const SearchBar = () => {
   const [searchResult, setSearchResult] = useState([]);
+  const [totalResults, setTotalResults] = useState(0);
   const searchApi = useApi(search);
 
   const handleQuery = useCallback(
     async (query) => {
       if (!query) return;
       const response = await searchApi.request(query);
-      if (response.data.Response === "True")
+      if (response.data.Response === "True") {
         setSearchResult(response.data.Search);
-      else setSearchResult([]);
+        setTotalResults(parseInt(response.data.totalResults));
+      } else {
+        setSearchResult([]);
+        setTotalResults(0);
+      }
     },
     [searchApi]
   );
 
   return (
     <div className={styles.searchbar}>
-      <label>Movie Title</label>
+      <div className={styles.searchbar_header}>
+        <label>Movie Title</label>
+        {totalResults > 0 && <span>Total Results: {totalResults}</span>}
+      </div>
       <SearchField handleQuery={handleQuery} searchResult={searchResult} />
     </div>
   );

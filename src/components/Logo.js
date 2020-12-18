3af3065508c8d71bdbf7118c91bf50705285f036
danielkanangila <daniel.kanangila@gmail.com>
2020-12-17 20:10:29 -0500

    added style files and create main, logo components

---
+++ src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Logo = () => {
+  return <LogoText>The Shoppies</LogoText>;
+};
+
+const LogoText = styled.h1``;
+
+export default Logo;
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./../styles/main.scss";
+import Logo from "./Logo";
+const Main = () => {
+  return (
+    <div className={styles.main_container}>
+      <Logo />
+    </div>
+  );
+};
+
+export default Main;

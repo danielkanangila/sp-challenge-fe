c1b84d1faf52d0aed6fcda242b5dd9f0867cf423
danielkanangila <daniel.kanangila@gmail.com>
2020-12-19 11:52:50 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import { SearchContextProvider } from "./context/search-context";
 import Notifications from "./components/Notifications";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <Home />
         <Notifications />
+        <Footer />
       </div>
     </SearchContextProvider>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./../styles/footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <h5>
+        Shopify UX Developer Intern & Web Developer Intern Challenge - Summer
+        2021
+      </h5>
+      <a href="https://kkanangila.com" target="_blank" rel="noreferrer">
+        By Kabandangi K. Kanangila - 2020
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
